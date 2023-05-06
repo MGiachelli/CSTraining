331308650459911a5824e2c2bacf0dcf82be7770
massimo_giachelli <mgiachelli@gmail.com>
2023-05-06 20:40:50 +0300

    PersonList added (TO DO)

--- C#/CSTraining/SerializationTask/Person.cs
+++ C#/CSTraining/SerializationTask/Person.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 namespace SerializationTask
 {
-    public class Person: Human
+    public class Person : Human
     {
-        public const int  MAX_CHILDREN = 5;
-        public const int  MAX_CARDS = 5;
-        public const int  MAX_PHONES = 5;
+        public const int MAX_CHILDREN = 5;
+        public const int MAX_CARDS = 5;
+        public const int MAX_PHONES = 5;
 
         public Guid TransportId { get; set; }
         public Int32 SequenceId { get; set; }
@@ -14,6 +14,17 @@ namespace SerializationTask
         public Double Salary { get; set; }
         public Boolean IsMarried { get; set; }
         public Human[]? Children { get; set; }
+
+        public Int32 AverageChildrenAge 
+        { get 
+            {
+                if (Children == null) return 0;
+                if (Children.Length == 0) return 0;
+                Int32 totalAge = 0;
+                foreach (var child in Children) { totalAge += child.Age ?? 0; };
+                return totalAge/Children.Length;
+            } 
+        }
 
         public override string? ToString()
         {
---
+++ C#/CSTraining/SerializationTask/PersonList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationTask
+{
+    public class PersonList : List<Person>
+    {
+        public PersonList()
+        {
+        }
+    }
+}
